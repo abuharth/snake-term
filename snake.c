cb3de08a240753980e570802a60b3ea92024fd64
Ahmed Abuharthieh <145587343+abuharth@users.noreply.github.com>
2024-02-14 04:19:03 -0500

    Update snake.c

--- snake.c
+++ snake.c
@@ -1,74 +1,114 @@
-#include <stdio.h>
-#include <string.h>
-#include <unistd.h>
 #include <stdlib.h>
-#include <stdbool.h>
+#include <unistd.h>
 #include <curses.h>
+#include <time.h>
+#include <string.h>
 
-#define MAX_LENGTH 256
+#define MAX_SCORE 256
 #define FRAME_TIME 110000
 
 typedef struct {
     int x;
     int y;
-} vec2d;
+} vec2;
 
 int score = 0;
-vec2d segments[MAX_LENGTH];
+char score_message[16];
 
-bool collide_snake(vec2d a) {
+bool skip = false;
+bool is_running = true;
+
+int screen_width = 25;
+int screen_height = 20;
+
+// initialize screen
+WINDOW *win;
+
+// snake
+vec2 head = { 0, 0 };
+vec2 segments[MAX_SCORE + 1];
+vec2 dir = { 1, 0 };
+// berry
+vec2 berry;
+
+
+bool collide(vec2 a, vec2 b) {
+    if (a.x == b.x && a.y == b.y) {
+        return true;
+    }
+    else return false;
+}
+
+bool collide_snake_body(vec2 point) {
     for (int i = 0; i < score; i++) {
-        if (a.x == segments[i].x && a.y == segments[i].y) {
+        if (collide(point, segments[i])) {
             return true;
         }
     }
     return false;
 }
 
-void draw_border(int width, int height, char c) {
-    for (int i = 0; i < width; i++) {
-        mvaddch(0, i * 2, c);
+vec2 spawn_berry() {
+    // spawn a new berry with 1 pixel padding from edges and not inside of the snake
+    vec2 berry = { 1 + rand() % (screen_width - 2), 1 + rand() % (screen_height - 2) };
+    while (collide(head, berry) || collide_snake_body(berry)) {
+        berry.x = 1 + rand() % (screen_width - 2);
+        berry.y = 1 + rand() % (screen_height - 2);
     }
-    for (int i = 1; i < height; i++) {
-        mvaddch(i, 0, c);
-        mvaddch(i, (width -1) * 2, c);
+    return berry;
+}
+
+void draw_border(int y, int x, int width, int height) {
+    // top row
+    mvaddch(y, x, ACS_ULCORNER);
+    mvaddch(y, x + width * 2 + 1, ACS_URCORNER);
+    for (int i = 1; i < width * 2 + 1; i++) {
+        mvaddch(y, x + i, ACS_HLINE);
     }
-    for (int i = 0; i < width; i++) {
-        mvaddch(height - 1, i * 2, c);
+    // vertical lines
+    for (int i = 1; i < height + 1; i++) {
+        mvaddch(y + i, x, ACS_VLINE);
+        mvaddch(y + i, x + width * 2 + 1, ACS_VLINE);
+    }
+    // bottom row
+    mvaddch(y + height + 1, x, ACS_LLCORNER);
+    mvaddch(y + height + 1, x + width * 2 + 1, ACS_LRCORNER);
+    for (int i = 1; i < width * 2 + 1; i++) {
+        mvaddch(y + height + 1, x + i, ACS_HLINE);
     }
 }
 
-int main(int argc, char *argv[]) {
-    // process command line args
-    bool looping;
-    if (argc == 1) {
-        looping = false;
-    }
-    else if (argc == 2) {
-        if (!strcmp(argv[1], "-h") || !strcmp(argv[1], "--help")) {
-            printf("usage: snake <argument>\n-l:\t\tlooping\n[DEFAULT]:\tno looping\n");
-            exit(0);
-        }
-        else if (!strcmp(argv[1], "-l")) {
-            looping = true;
-        }
-    }
+void quit_game() {
+    // exit cleanly from application
+    endwin();
+    // clear screen, place cursor on top, and un-hide cursor
+    printf("\e[1;1H\e[2J");
+    printf("\e[?25h");
 
-    WINDOW *win = initscr();
+    exit(0);
+}
 
-    int screen_width;
-    int screen_height;
+void restart_game() {
+    head.x = 0;
+    head.y = 0;
+    dir.x = 1;
+    dir.y = 0;
+    score = 0;
+    sprintf(score_message, "[ Score: %d ]", score);
+    is_running = true;
+}
 
-    getmaxyx(win, screen_height, screen_width);
-    if (screen_width % 2 == 0) {
-        screen_width = (screen_width / 2) - 2;
-    }
-    else {
-        screen_width = (screen_width / 2) - 1;
-    }
-    screen_height -= 2;
+void init() {
+    srand(time(NULL));
+    // initialize window
+    win = initscr();
+    // take player input and hide cursor
+    keypad(win, true);
+    noecho();
+    nodelay(win, true);
+    curs_set(0);
 
-    // setting up colors
+    // initialize color
     if (has_colors() == FALSE) {
         endwin();
         fprintf(stderr, "Your terminal does not support color\n");
@@ -78,191 +118,158 @@ int main(int argc, char *argv[]) {
     use_default_colors();
     init_pair(1, COLOR_RED, -1);
     init_pair(2, COLOR_GREEN, -1);
+    init_pair(3, COLOR_YELLOW, -1);
 
-    // take input, non-blocking, and hide cursor
-    keypad(win, true);
-    nodelay(win, true);
-    curs_set(0);
 
-    vec2d head = { 0, 0 };
-    vec2d dir = { 1, 0 };
-    char head_char = '>';
+    berry.x = rand() % screen_width;
+    berry.y = rand() % screen_height;
 
-    bool spawn_berry = false;
-    vec2d berries[32];
-    int num_berries = 1;
+    // update score message
+    sprintf(score_message, "[ Score: %d ]", score);
+}
 
-    vec2d berry = { 1 + rand() % (screen_width - 1), 1 + rand() % (screen_height - 2) };
-    berries[0] = berry;
-
-    char score_message[32];
-    sprintf(score_message, "  Score: %d  ", score);
-
-    while(true) {
-        // reactive screen
-        getmaxyx(win, screen_height, screen_width);
-        if (screen_width % 2 == 0) {
-            screen_width = (screen_width / 2) - 2;
+void process_input() {
+    int pressed = wgetch(win);
+    if (pressed == KEY_LEFT) {
+        if (dir.x == 1) {
+            return;
+            skip = true;
         }
-        else {
-            screen_width = (screen_width / 2) - 1;
+        dir.x = -1;
+        dir.y = 0;
+    }
+    if (pressed == KEY_RIGHT) {
+        if (dir.x == -1) {
+            return;
+            skip = true;
         }
-        screen_height -= 2;
-
-        int pressed = wgetch(win);
-        if (pressed == KEY_LEFT) {
-            if (dir.x == 1) continue;
-            dir.x = -1;
-            dir.y = 0;
-            head_char = '<';
+        dir.x = 1;
+        dir.y = 0;
+    }
+    if (pressed == KEY_UP) {
+        if (dir.y == 1) {
+            return;
+            skip = true;
         }
-        if (pressed == KEY_RIGHT) {
-            if (dir.x == -1) continue;
-            dir.x = 1;
-            dir.y = 0;
-            head_char = '>';
+        dir.x = 0;
+        dir.y = -1;
+    }
+    if (pressed == KEY_DOWN) {
+        if (dir.y == -1) {
+            return;
+            skip = true;
         }
-        if (pressed == KEY_UP) {
-            if (dir.y == 1) continue;
-            dir.x = 0;
-            dir.y = -1;
-            head_char = '^';
-        }
-        if (pressed == KEY_DOWN) {
-            if (dir.y == -1) continue;
-            dir.x = 0;
-            dir.y = 1;
-            head_char = 'v';
-        }
-        if (pressed == '\e') {
-            break;
-        }
+        dir.x = 0;
+        dir.y = 1;
+    }
+    if (pressed == ' ') {
+        if (!is_running)
+            restart_game();
+    }
+    if (pressed == '\e') {
+        is_running = false;
+        quit_game();
+    }
+}
 
-        // update snake segments
-        for (int i = score; i > 0; i--) {
-            segments[i] = segments[i - 1];
-        }
+void game_over() {
+    while (is_running == false) {
+        process_input();
 
-        segments[0] = head;
-
-        head.x += dir.x;
-        head.y += dir.y;
-
-        // loops through play area
-        if (looping) {
-            if (head.x < 0) {
-                head.x = screen_width;
-            }
-            if (head.x > screen_width) {
-                head.x = 0;
-            }
-            if (head.y < 0) {
-                head.y = screen_height - 1;
-            }
-            if (head.y > screen_height - 1) {
-                head.y = 0;
-            }
-        }
-
-        // keep berry on screen when resizing
-        for (int i = 0; i < num_berries; i++) {
-            if (berries[i].x > screen_width){
-                berries[i].x = screen_width;
-            }
-            if (berries[i].y > screen_height - 1) {
-                berries[i].y = screen_height - 1;
-            }
-        }
-
-        erase();
-
-        // draw berries
-        for (int i = 0; i < num_berries; i ++) {
-            attron(COLOR_PAIR(1));
-            mvaddch(berries[i].y + 1, berries[i].x * 2 + 1, '@');
-            attroff(COLOR_PAIR(1));
-        }
-
-        // draw snake
-        attron(COLOR_PAIR(2));
-        for (int i = 0; i < score; i++) {
-            mvaddch(segments[i].y + 1, segments[i].x * 2 + 1, 'o');
-        }
-        mvaddch(head.y + 1, head.x * 2 + 1, head_char);
-        attroff(COLOR_PAIR(2));
-
-        draw_border(screen_width + 2, screen_height + 2, '#');
-
-        mvaddstr(0, screen_width - 5, score_message);
-
-        // game over
-        if (collide_snake(head) || head.x < 0 || head.x > screen_width || head.y < 0 || head.y > screen_height - 1) {
-            while(true) {
-                int pressed = wgetch(win);
-                if (pressed == ' ') {
-                    score = 0;
-                    num_berries = 1;
-                    head.x = 0;
-                    head.y = 0;
-                    dir.x = 1;
-                    dir.y = 0;
-                    head_char = '>';
-                    sprintf(score_message, "  Score: %d  ", score);
-                    break;
-                }
-                if (pressed == '\e') {
-                    endwin();
-                    exit(0);
-                }
-
-                // GAME OVER text
-                attron(COLOR_PAIR(1));
-                mvaddstr(screen_height - 2, screen_width - 3, "GAME OVER");
-                mvaddstr(screen_height - 1, screen_width - 15, "[SPACE] = RESTART | [ESC] = EXIT");
-                attroff(COLOR_PAIR(1));
-
-                mvaddstr(0, screen_width - 5, score_message);
-                
-                usleep(FRAME_TIME);
-            }
-        }
-
-        for (int i = 0; i < num_berries; i ++) {
-            if (berries[i].x == head.x && berries[i].y == head.y) {
-                if (score < MAX_LENGTH) {
-                    score += 1;
-                    if (score % 10 == 0) {
-                        spawn_berry = true;
-                    }
-                }
-                else {
-                    // WIN!
-                }
-                sprintf(score_message, "  Score: %d  ", score);
-
-                while(collide_snake(berries[i]) || (berries[i].x == head.x && berries[i].y == head.y)) {
-                    berries[i].x = 1 + rand() % (screen_width - 1);
-                    berries[i].y = 1 + rand() % (screen_height - 2);
-                }
-            }
-        }
-
-        if (spawn_berry == true) {
-            num_berries += 1;
-            vec2d new_berry = { 1 + rand() % (screen_width - 1), 1 + rand() % (screen_height - 2) };
-            while(collide_snake(new_berry) || (new_berry.x == head.x && new_berry.y == head.y)) {
-                new_berry.x = 1 + rand() % (screen_width - 1);
-                new_berry.y = 1 + rand() % (screen_height - 2);
-            }
-            berries[num_berries - 1] = new_berry;
-            spawn_berry = false;
-        }
+        mvaddstr(screen_height / 2, screen_width - 17, "             Game Over          ");
+        mvaddstr(screen_height / 2 + 1, screen_width - 17, "[SPACE] to restart, [ESC] to quit");
+        attron(COLOR_PAIR(3));
+        draw_border(screen_height / 2 - 1, screen_width - 18, 17, 2);
+        attroff(COLOR_PAIR(3));
 
         usleep(FRAME_TIME);
+    }
+}
 
+void update() {
+    // update snake segments
+    for (int i = score; i > 0; i--) {
+        segments[i] = segments[i - 1];
+    }
+    segments[0] = head;
+
+    // move snake
+    head.x += dir.x;
+    head.y += dir.y;
+
+    // collide with body or walls
+    if (collide_snake_body(head) || head.x < 0 || head.y < 0 \
+            || head.x >= screen_width || head.y >= screen_height) {
+        is_running = false;
+        game_over();
     }
 
-    endwin();
+    // eating a berry
+    if (collide(head, berry)) {
+        if (score < MAX_SCORE) {
+            score += 1;
+            sprintf(score_message, "[ Score: %d ]", score);
+        }
+        else {
+            // WIN!
+            printf("You Win!");
+        }
+        berry = spawn_berry();
+    }
 
+    usleep(FRAME_TIME);
+}
+
+void draw() {
+    erase();
+
+    attron(COLOR_PAIR(1));
+    mvaddch(berry.y+1, berry.x * 2+1, '@');
+    attroff(COLOR_PAIR(1));
+
+    // draw snake
+    attron(COLOR_PAIR(2));
+    for (int i = 0; i < score; i++) {
+        mvaddch(segments[i].y+1, segments[i].x * 2 + 1, ACS_DIAMOND);
+    }
+    mvaddch(head.y+1, head.x * 2+1, 'O');
+    attroff(COLOR_PAIR(2));
+
+    attron(COLOR_PAIR(3));
+    draw_border(0, 0, screen_width, screen_height);
+    attroff(COLOR_PAIR(3));
+    mvaddstr(0, screen_width - 6, score_message);
+}
+
+int main(int argc, char *argv[]) {
+    // process user args
+    if (argc == 1) {}
+    else if (argc == 3) {
+        if (!strcmp(argv[1], "-d")) {
+            if (sscanf(argv[2], "%dx%d", &screen_width, &screen_height) != 2) {
+                printf("Usage: snake [options]\nOptions:\n -d [width]x[height]"
+                       "\tdefine dimensions of the screen\n\nDefault dimensions are 25x20\n");
+                exit(1);
+            }
+        }
+    }
+    else {
+        printf("Usage: snake [options]\nOptions:\n -d [width]x[height]"
+               "\tdefine dimensions of the screen\n\nDefault dimensions are 25x20\n");
+        exit(1);
+    }
+
+    init();
+    while(is_running) {
+        process_input();
+        if (skip == true) {
+            skip = false;
+            continue;
+        }
+
+        update();
+        draw();
+    }
+    quit_game();
     return 0;
 }
